270384c31857c562c0686f1a7aedca2911dbca45
tithi2149 <tanyasultana60@gmail.com>
2023-11-08 22:38:35 +0600

    added the framer motion

--- src/Component/Pages/Homepage/Banner/Banner.jsx
+++ src/Component/Pages/Homepage/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from "framer-motion"
 
 const Banner = () => {
     return (
@@ -6,9 +7,17 @@ const Banner = () => {
         <div className="hero-overlay bg-opacity-20"></div>
         <div className="hero-content lg:absolute lg:left-60 text-center max-w-md text-neutral-content">
           <div className="">
-            <h1 className="mb-5 text-blue-600 text-5xl font-bold">Unleashing the Magic of Books!!!</h1>
+
+          <motion.h1
+    initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }}
+    className="mb-5 text-blue-600 text-5xl font-bold">  Unleashing the Magic of Books!!! </motion.h1> 
+          
             <p className="mb-5 text-black font-semibold">Explore the Literary World with Reviews, Recommendations, and Author Insights</p>
             <button className="px-4 py-2 rounded-lg bg-red-500 text-white">Discover Now!</button>
+          
+           
           </div>
         </div>
       </div>
